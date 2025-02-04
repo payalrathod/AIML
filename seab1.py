e6476f449709045a1b65619ab719621a2d5b570d
Payal Rathod <76880045+payalrathod@users.noreply.github.com>
2025-02-04 23:48:38 +0530

    Add files via upload

---
+++ seab1.py
@@ -0,0 +1,15 @@
+import numpy as np
+from numpy import array
+import pandas as pd
+from io import StringIO, BytesIO
+from typing import List
+import numpy as np
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+from scipy import stats
+print(sns.get_dataset_names('tips.csv' on https://github.com/mwaskom/seaborn-data))
+
+
+
